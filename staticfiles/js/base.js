f2acc74e6daad96c857a46f2e9142e205bba635f
PaPpY-OG <adebayogoodness258@gmail.com>
2026-05-31 12:13:02 +0100

    updated templates

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,14 +1,17 @@
-function toggleMenu() {
-    document.querySelector('.sidebar').classList.toggle('active');
-  }
-function toggleMenu() {
-    document.querySelector('.sidebar').classList.toggle('active');
+  function toggleMenu() {
+      const sidebar = document.querySelector('.sidebar');
+      const menuIcon = document.querySelector('.menu-icon');
+
+      sidebar.classList.toggle('active');
+      menuIcon.classList.toggle('hidden');
   }
 
   document.addEventListener('click', function(e) {
-    const sidebar = document.querySelector('.sidebar');
-    const menuIcon = document.querySelector('.menu-icon');
-    if (!sidebar.contains(e.target) && !menuIcon.contains(e.target)) {
-      sidebar.classList.remove('active');
-    }
+      const sidebar = document.querySelector('.sidebar');
+      const menuIcon = document.querySelector('.menu-icon');
+
+      if (!sidebar.contains(e.target) && !menuIcon.contains(e.target)) {
+          sidebar.classList.remove('active');
+          menuIcon.classList.remove('hidden');
+      }
   });
